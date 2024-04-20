87c28336759a5a9bcaf38c4a6b753e1f86983da6
nicetry001 <arwin00139.aa@gmail.com>
2024-04-21 03:06:58 +0800

    implementing lambda+api gw with cors

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,12 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://gst6lziagf.execute-api.us-east-1.amazonaws.com/prod");
+    let data = await response.json();
+    counter.innerHTML = ` Views:${data}`;
+}
+
+updateCounter();
